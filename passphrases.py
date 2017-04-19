81403c9ef8e64896a896a14a04bb8639ec4bf4d5
B. J. Potter <bjpotter@gmail.com>
2017-04-19 16:40:31 -0500

    Update for use as a module

--- passphrases.py
+++ passphrases.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2012 B. J. Potter
+# Copyright 2017 B. J. Potter
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,13 +23,14 @@ words. The default and suggested usage is to pick a pass phrase of 4
 random words from a list of 10,000 words.
 """
 
+from __future__ import print_function
 import os
 import sys
 import random
 from optparse import OptionParser
 
 
-def pass_phrases(
+def generate_pass_phrases(
     word_list,
     number_words_in_phrase,
     number_phrases):
@@ -44,31 +45,52 @@ def pass_phrases(
     integer_max = len(word_list)
     # start with the first (0th) word
     integer_min = 0
+    pass_phrases = []
     for _ in range(number_phrases):
+        pass_phrase = []
         for _ in range(number_words_in_phrase):
             # SystemRandom is a cryptographically secure way
             # to generate random numbers.
-            print word_list[random.SystemRandom().randint(
-                integer_min, integer_max)],
-        print
+            pass_phrase.append(
+                word_list[random.SystemRandom().randint(
+                    integer_min, integer_max)])
+        pass_phrases.append(pass_phrase)
+    return pass_phrases
 
 
-# Running from command line (main)
-usage = "usage: %prog [options]"
-parser = OptionParser(usage)
-path_to_script = os.path.dirname(os.path.abspath(__file__))
-default_file = os.path.join(path_to_script, 'word_list.txt')
-parser.add_option("-w", "--words", default=4,
-    help="Number of words per phrase")
-parser.add_option("-n", "--num", default=5,
-    help="Number of phrases")
-parser.add_option("-f", "--file", default=default_file,
-    help="Use other than the default word list")
+def print_pass_phrases(pass_phrases):
+    for pass_phrase in pass_phrases:
+        printable_pass_phrase = ' '.join(pass_phrase)
+        print(printable_pass_phrase)
 
-(options, args) = parser.parse_args()
-if os.path.isfile(options.file):
-    word_list = list(line.strip() for line in open(options.file))
-else:
-    sys.stdout.write('%s is not a file' % options.file)
-    sys.exit(1)
-pass_phrases(word_list, int(options.words), int(options.num))
+
+def get_word_list(file_name):
+    with open(file_name) as input_file:
+        word_list = [line.strip() for line in input_file]
+    return word_list
+
+
+def command_line():
+    """Running from command line (main)"""
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage)
+    path_to_script = os.path.dirname(os.path.abspath(__file__))
+    default_file = os.path.join(path_to_script, 'word_list.txt')
+    parser.add_option("-w", "--words", default=4,
+        help="Number of words per phrase")
+    parser.add_option("-n", "--num", default=5,
+        help="Number of phrases")
+    parser.add_option("-f", "--file", default=default_file,
+        help="Use other than the default word list")
+    (options, args) = parser.parse_args()
+    if os.path.isfile(options.file):
+        word_list = get_word_list(options.file)
+    else:
+        sys.stdout.write('%s is not a file' % options.file)
+        sys.exit(1)
+    print_pass_phrases(
+        generate_pass_phrases(
+            word_list, int(options.words), int(options.num)))
+
+if __name__ == '__main__':
+    command_line()
